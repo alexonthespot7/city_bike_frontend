487ec7ca6c206ce5a6ff75a0d992615454109e5d
unknown <bgw595@myy.haaga-helia.fi>
2023-05-30 11:54:51 +0300

    Filtering, sorting and pagination added for Journeys

--- src/Components/Journeys.js
+++ src/Components/Journeys.js
@@ -13,45 +13,78 @@ import { Link } from "react-router-dom";
 import useMediaQuery from "../Hooks/useMediaQuery";
 
 function Journeys() {
-    const [journeys, setJourneys] = useState([]);
     const [dataFetched, setDataFetched] = useState(false);
+    const [gridApi, setGridApi] = useState(null);
 
     const matchesM = useMediaQuery("(min-width: 650px)");
 
     const gridRef = useRef();
 
-    const fetchJourneys = () => {
-        fetch(process.env.REACT_APP_API_URL + '/journeys')
-            .then(response => response.json())
-            .then(data => {
-                setJourneys(data);
-                setDataFetched(true);
-            })
-            .catch(err => console.error(err));
-    }
-
-    useEffect(() => {
-        fetchJourneys();
+    const onGridReady = useCallback((params) => {
+        setGridApi(params.api);
     }, []);
 
+    useEffect(() => {
+        if (gridApi) {
+            setDataFetched(false);
+            const dataSource = {
+                getRows: (params) => {
+                    const page = params.endRow / 100;
+                    console.log(params.filterModel["departureStation.name"]);
+                    console.log(process.env.REACT_APP_API_URL +
+                        `/journeys?page=${page - 1}` +
+                        (params.sortModel.length > 0 ? `&sort=${params.sortModel[0].colId}&order=${params.sortModel[0].sort}` : '') +
+                        (params.filterModel["departureStation.name"] ? `depValue=${params.filterModel["departureStation.name"].filter}&depOperation=${params.filterModel["departureStation.name"].type}` : '') +
+                        (params.filterModel["returnStation.name"] ? `retValue=${params.filterModel['returnStation.name'].filter}&retOperation=${params.filterModel['returnStation.name'].type}` : '') +
+                        (params.filterModel.distance ? `distValue=${params.filterModel.distance.filter}&distOperation=${params.filterModel.distance.type}` : '') +
+                        (params.filterModel.duration ? `durValue=${params.filterModel.duration.filter}&durOperation=${params.filterModel.duration.type}` : ''));
+                    fetch(process.env.REACT_APP_API_URL +
+                        `/journeys?page=${page - 1}` +
+                        (params.sortModel.length > 0 ? `&sort=${params.sortModel[0].colId}&order=${params.sortModel[0].sort}` : '') +
+                        (params.filterModel["departureStation.name"] ? `&depValue=${params.filterModel["departureStation.name"].filter}&depOperation=${params.filterModel["departureStation.name"].type}` : '') +
+                        (params.filterModel["returnStation.name"] ? `&retValue=${params.filterModel['returnStation.name'].filter}&retOperation=${params.filterModel['returnStation.name'].type}` : '') +
+                        (params.filterModel.distance ? `&distValue=${params.filterModel.distance.filter}&distOperation=${params.filterModel.distance.type}` : '') +
+                        (params.filterModel.duration ? `&durValue=${params.filterModel.duration.filter}&durOperation=${params.filterModel.duration.type}` : ''))
+                        .then(resp => resp.json())
+                        .then(data => {
+                            setDataFetched(true);
+                            params.successCallback(data.content, data.totalElements);
+                        }).catch(err => {
+                            params.successCallback([], 0);
+                        });
+                }
+            }
+
+            gridApi.setDatasource(dataSource);
+        }
+    }, [gridApi]);
+
     const durationFormatter = (params) => {
-        return `${Math.floor(params.value / 60)} min ${params.value % 60} sec`;
+        if (params.value) {
+            return `${Math.floor(params.value / 60)} min ${params.value % 60} sec`;
+        } else {
+            return '';
+        }
     }
 
     const distanceFormatter = (params) => {
-        return `${Math.floor(params.value / 1000)} km ${params.value % 1000} m`;
+        if (params.value) {
+            return `${Math.floor(params.value / 1000)} km ${params.value % 1000} m`;
+        } else {
+            return '';
+        }
     }
 
     const [columns, setColumns] = useState([
         {
             headerName: 'Departure station',
-            cellRenderer: params => <Link style={{ color: '#050404' }} to={'../stations/' + params.data.departureStation.id} >{params.data.departureStation.name}</Link>,
+            cellRenderer: params => <Link style={{ color: '#050404' }} to={`../stations/'${params.data ? params.data.departureStation.id : ''}`} >{(params.data) ? params.data.departureStation.name : ''}</Link>,
             field: 'departureStation.name',
             type: 'stations'
         },
         {
             headerName: 'Return station',
-            cellRenderer: params => <Link style={{ color: '#050404' }} to={'../stations/' + params.data.returnStation.id} >{params.data.returnStation.name}</Link>,
+            cellRenderer: params => <Link style={{ color: '#050404' }} to={`../stations/${(params.data) ? params.data.returnStation.id : ''}`} >{(params.data) ? params.data.returnStation.name : ''}</Link>,
             field: 'returnStation.name',
             type: 'stations'
         },
@@ -71,6 +104,10 @@ function Journeys() {
         return {
             width: 300,
             filter: 'agTextColumnFilter',
+            filterParams: {
+                suppressAndOrCondition: true,
+                filterOptions: ['contains', 'equals'],
+            },
             resizable: true,
             sortable: true,
             cellStyle: { 'text-align': 'left' }
@@ -79,15 +116,16 @@ function Journeys() {
 
     const columnTypes = useMemo(() => {
         return {
-            narrow: { width: 190, filter: 'agNumberColumnFilter' },
+            narrow: {
+                width: 190,
+                filter: 'agNumberColumnFilter',
+                filterParams: {
+                    suppressAndOrCondition: true,
+                    filterOptions: ['equals', 'lessThan', 'greaterThan']
+                }
+            },
             stations: { cellStyle: {} }
         }
-    }, []);
-
-    const onFilterTextBoxChanged = useCallback(() => {
-        gridRef.current.api.setQuickFilter(
-            document.getElementById('filter-text-box').value
-        );
     }, []);
 
     return (
@@ -98,38 +136,23 @@ function Journeys() {
             style={{ display: 'flex', flexDirection: 'column', gap: 15, alignItems: 'center', justifyContent: 'center' }}
         >
             <Typography variant={matchesM ? 'h5' : 'h6'}>Journeys</Typography>
-            {dataFetched &&
-                <div className="ag-theme-material" style={{ height: 600, width: '80%', maxWidth: 1000, margin: 'auto', display: 'flex', flexDirection: 'column', gap: 10 }}>
-                    <TextField
-                        type='search'
-                        fullWidth={false}
-                        size='small'
-                        id="filter-text-box"
-                        placeholder="Search for journeys..."
-                        InputProps={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <SearchIcon />
-                                </InputAdornment>
-                            ),
-                        }}
-                        variant="standard"
-                        onChange={onFilterTextBoxChanged}
-                    />
-                    <AgGridReact
-                        ref={gridRef}
-                        defaultColDef={defaultColDef}
-                        columnTypes={columnTypes}
-                        columnDefs={columns}
-                        rowData={journeys}
-                        pagination={true}
-                        paginationPageSize={20}
-                        suppressCellFocus={true}
-                        animateRows="true"
-                    />
-                </div>
-            }
-            {!dataFetched && <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', marginTop: '20vh', marginBottom: '55vh' }}><CircularProgress color="fourth" /></div>}
+            <div className="ag-theme-material" style={{ height: 600, width: '80%', maxWidth: 1000, margin: 'auto', display: 'flex', flexDirection: 'column', gap: 10 }}>
+                <AgGridReact
+                    ref={gridRef}
+                    defaultColDef={defaultColDef}
+                    columnTypes={columnTypes}
+                    columnDefs={dataFetched ? columns : null}
+                    pagination={true}
+                    paginationPageSize={100}
+                    suppressCellFocus={true}
+                    animateRows="true"
+                    onGridReady={onGridReady}
+                    cacheBlockSize={100}
+                    rowModelType={'infinite'}
+                >
+                    {!dataFetched && <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', marginTop: '20vh', marginBottom: '55vh' }}><CircularProgress color="fourth" /></div>}
+                </AgGridReact>
+            </div>
         </motion.div >
     );
 }
